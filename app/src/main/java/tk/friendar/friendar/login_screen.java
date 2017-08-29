d2ca2114c26f33cbb382c0db7958633ba9a80e11
sdi2 <sdi2@student.unimelb.edu.au>
2017-08-29 13:09:29 +1000

    Simons UI

---
+++ app/src/main/java/tk/friendar/friendar/login_screen.java
@@ -0,0 +1,22 @@
+package tk.friendar.friendar;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by Simon on 8/23/2017.
+ */
+
+public class login_screen extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+
+}
---
+++ app/src/main/java/tk/friendar/friendar/signUp.java
@@ -0,0 +1,19 @@
+package tk.friendar.friendar;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+/**
+ * Created by Simon on 8/22/2017.
+ */
+
+public class signUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
