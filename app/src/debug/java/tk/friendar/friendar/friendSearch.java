282732525dbc4029b8f74f8d0e89d32597f3174f
sdi2 <sdi2@student.unimelb.edu.au>
2017-09-12 13:21:46 +1000

    signup layout fix

--- app/src/debug/java/tk/friendar/friendar/friendSearch.java
+++ app/src/debug/java/tk/friendar/friendar/friendSearch.java
@@ -19,13 +19,14 @@ public class friendSearch extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_search);
         ListView listview = (ListView)findViewById(R.id.FriendList);
+        /*
         ArrayList<String> arrayFriend = new ArrayList<>();
         arrayFriend.addAll(Arrays.asList(getResources().getStringArray(R.array.friend_array)));
 
         adapter = new ArrayAdapter<>(friendSearch.this, android.R.layout.simple_list_item_1,arrayFriend);
 
         listview.setAdapter(adapter);
-
+        */
     }
 
 
