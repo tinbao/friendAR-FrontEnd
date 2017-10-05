116e4d1fdbd2a91f1dbdabc76f31c355d210d762
Travis <travis@travis-ci.org>
2017-10-06 01:21:30 +1100

    finished rest client

--- app/src/main/java/tk/friendar/friendar/Login.java
+++ app/src/main/java/tk/friendar/friendar/Login.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 /**
  * Created by Simon on 8/22/2017.
@@ -12,25 +11,14 @@ import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
-    Button signUp;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        signUp = (Button) findViewById(R.id.button);
-        signUp.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                signUp();
-            }
-        });
     }
 
-    public void signUp(){
+    public void signUp(View view){
 
         Intent intent_signup = new Intent(this,signUp.class);
         startActivity(intent_signup);
@@ -38,7 +26,7 @@ public class Login extends AppCompatActivity {
 
 
     }
-    public void login(){
+    public void login(View view){
         Intent intent_login = new Intent(this,login_screen.class);
         startActivity(intent_login);
 
--- app/src/main/java/tk/friendar/friendar/signUp.java
+++ app/src/main/java/tk/friendar/friendar/signUp.java
@@ -17,15 +17,21 @@ import android.widget.TextView;
 import android.support.v4.app.DialogFragment;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.util.Calendar;
 
@@ -39,54 +45,34 @@ import java.util.Map;
 
 public class signUp extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
-//    EditText userEmail, userPassword, confirmPassword;
-//    ProgressBar progressBar;
-//    Button complete;
-
+    EditText userEmail, userPassword, confirmPassword;
+    ProgressBar progressBar;
+    Button complete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
-//
-//        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-//        complete = (Button) findViewById(R.id.complete_signup);
-//
-//        //if the user is already logged in we will directly start the profile activity
-//        /*if (SharedPrefManager.getInstance(this).isLoggedIn()) {
-//            finish();
-//            startActivity(new Intent(this, ProfileActivity.class));
-//            return;
-//        }*/
-//
-//        userEmail = (EditText) findViewById(R.id.email);
-//        userPassword = (EditText) findViewById(R.id.password);
-//        confirmPassword = (EditText) findViewById(R.id.password2);
-//
-//        /* For sign up button */
-//        complete.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //if user pressed on button register
-//                //here we will register the user to server
-//                //registerUser();
-//            }
-//        });
-//
-//        /* For login button */
-//        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //if user pressed on login
-//                //we will open the login screen
-//                finish();
-//                startActivity(new Intent(signUp.this, LoginActivity.class));
-//            }
-//        });
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        complete = (Button) findViewById(R.id.complete_signup);
+
+        userEmail = (EditText) findViewById(R.id.email);
+        userPassword = (EditText) findViewById(R.id.password);
+        confirmPassword = (EditText) findViewById(R.id.password2);
+
+        /* For sign up button */
+        complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* Creates JSON Object (String) and POSTs to server for new users */
+                register();
+            }
+        });
+
     }
 
     public void datePicker(View view){
-
         DatePickerFragment fragment = new DatePickerFragment();
         fragment.show(getSupportFragmentManager(),"date");
     }
@@ -96,6 +82,7 @@ public class signUp extends AppCompatActivity implements DatePickerDialog.OnDate
         ((TextView) findViewById(R.id.showDate)).setText(dateFormat.format(calendar.getTime()));
 
     }
+
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
         Calendar cal = new GregorianCalendar(year,month, dayOfMonth);
@@ -119,110 +106,179 @@ public class signUp extends AppCompatActivity implements DatePickerDialog.OnDate
     }
 
     public void signUp(View view){
-
         Intent intent_signup = new Intent(this,signUp.class);
         startActivity(intent_signup);
-
-
-
     }
+
     public void login(View view){
         Intent intent_login = new Intent(this,login_screen.class);
         startActivity(intent_login);
-
     }
 
+    /*
+     * Registers a new user to the database using volley's POST method through a REST API
+     * Will also confirm that the user enters in correctly formatted inputs.
+     */
+    private void register() {
+        final String email = userEmail.getText().toString().trim();
+        final String password = userPassword.getText().toString().trim();
+        final String password_confirm = confirmPassword.getText().toString().trim();
+        /* Temporary Username taking the string before the @ in the email */
+        final String userName = email.split("@")[0];
 
-//    private void registerUser() {
-//        final String email = userEmail.getText().toString().trim();
-//        final String password = userPassword.getText().toString().trim();
-//        final String password_confirm = confirmPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(email)) {
+            userEmail.setError("Please enter your email");
+            userEmail.requestFocus();
+            return;
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            userEmail.setError("Enter a valid email");
+            userEmail.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            userPassword.setError("Enter a password");
+            userPassword.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password_confirm)) {
+            confirmPassword.setError("Confirm a password");
+            confirmPassword.requestFocus();
+            return;
+        }
+
+        if (!password.equals(password_confirm)){
+            userPassword.setError("Passwords do not match");
+            userPassword.requestFocus();
+            confirmPassword.setError("Passwords do not match");
+            confirmPassword.requestFocus();
+            return;
+        }
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        JSONObject params = new JSONObject();
+        try {
+            params.put("fullName", userName);
+            params.put("email", email);
+            /* Temporary username, user can change it afterwards */
+            params.put("username", email);
+            params.put("usersPassword", password);
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        System.out.println(params.toString());
+
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, URLs.URL_SIGNUP, params,
+            new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        VolleyLog.v("Response:%n %s", response.toString(4));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },
+            new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    NetworkResponse response = error.networkResponse;
+                    if (error instanceof ServerError && response != null) {
+                        try {
+                            String res = new String(response.data,
+                                    HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                            // Now you can use any deserializer to make sense of data
+                            JSONObject obj = new JSONObject(res);
+                        } catch (UnsupportedEncodingException e1) {
+                            // Couldn't properly decode data to string
+                            e1.printStackTrace();
+                        } catch (JSONException e2) {
+                            // returned data is not JSONObject?
+                            e2.printStackTrace();
+                        }
+                    }
+                }
+            }){
+
+        };
+        requestQueue.add(req);
+
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST, URLs.URL_SIGNUP,
+//            new Response.Listener<String>() {
+//                @Override
+//                public void onResponse(String response) {
 //
-//        if (TextUtils.isEmpty(email)) {
-//            userEmail.setError("Please enter your email");
-//            userEmail.requestFocus();
-//            return;
-//        }
+//                    Context context = getApplicationContext();
+//                    CharSequence text = response;
+//                    int duration = Toast.LENGTH_SHORT;
 //
-//        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-//            userEmail.setError("Enter a valid email");
-//            userEmail.requestFocus();
-//            return;
-//        }
-//
-//        if (TextUtils.isEmpty(password)) {
-//            userPassword.setError("Enter a password");
-//            userPassword.requestFocus();
-//            return;
-//        }
-//
-//        if (TextUtils.isEmpty(password_confirm)) {
-//            confirmPassword.setError("Confirm a password");
-//            confirmPassword.requestFocus();
-//            return;
-//        }
-//
-//        if (!password.equals(password_confirm)){
-//            userPassword.setError("Passwords do not match");
-//            userPassword.requestFocus();
-//            confirmPassword.setError("Passwords do not match");
-//            confirmPassword.requestFocus();
-//            return;
-//        }
-//
-//        StringRequest stringRequest = new StringRequest(Request.Method.POST, URLs.URL_SIGNUP, new Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//                progressBar.setVisibility(View.GONE);
-//
-//                try {
-//                    //converting response to json object
-//                    JSONObject obj = new JSONObject(response);
-//
-//                    //if no error in response
-//                    if (!obj.getBoolean("error")) {
-//                        Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
-//
-//                        //getting the user from the response
-//                        JSONObject userJson = obj.getJSONObject("user");
-//
-//                        //creating a new user object
-//                        User user = new User(
-//                                userJson.getString("name"),
-//                                userJson.getString("username"),
-//                                userJson.getString("email")
-//                        );
-//
-//                        //storing the user in shared preferences
-//                        //SharedPrefManager.getInstance(getApplicationContext()).userLogin(user);
-//
-//                        //starting the profile activity
-//                        finish();
-//                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-//                    } else {
-//                        Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
-//                    }
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
+//                    Toast toast = Toast.makeText(context, text, duration);
+//                    toast.show();
 //                }
-//            }
-//        },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+//            },
+//
+//            new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//
+//                    NetworkResponse response = error.networkResponse;
+//                    if (error instanceof ServerError && response != null) {
+//                        try {
+//                            String res = new String(response.data,
+//                                    HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+//                            // Now you can use any deserializer to make sense of data
+//                            JSONObject obj = new JSONObject(res);
+//                        } catch (UnsupportedEncodingException e1) {
+//                            // Couldn't properly decode data to string
+//                            e1.printStackTrace();
+//                        } catch (JSONException e2) {
+//                            // returned data is not JSONObject?
+//                            e2.printStackTrace();
+//                        }
 //                    }
-//                }) {
+//
+//                    error.printStackTrace();
+//                    requestQueue.stop();
+//                }
+//        }){
+//
 //            @Override
-//            protected Map<String, String> getParams() throws AuthFailureError {
-//                Map<String, String> params = new HashMap<>();
-//                params.put("email", email);
-//                params.put("password", password);
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                Map<String,String> params = new HashMap<String, String>();
+//                params.put("Content-Type","application/x-www-form-urlencoded");
 //                return params;
+//            }
+//
+//            @Override
+//            public byte[] getBody(){
+//                HashMap<String, String> params = new HashMap<String, String>();
+//                params.put("fullName", userName);
+//                params.put("email", email);
+//                /* Temporary username, user can change it afterwards */
+//                params.put("username", email);
+//                params.put("usersPassword", password);
+//
+//                System.out.println("FROM BODY");
+//                System.out.println(new JSONObject(params).toString());
+//
+//                return new JSONObject(params).toString().getBytes();
+//            }
+//
+//            @Override
+//            public String getBodyContentType() {
+//                return "application/json";
 //            }
 //        };
 //
-//        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
-//    }
+//        stringRequest.setShouldCache(false);
+//        requestQueue.add(stringRequest);
+    }
 
 }
