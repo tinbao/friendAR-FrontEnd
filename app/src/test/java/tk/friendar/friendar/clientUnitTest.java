d3a783d016d1e5c5cdfffce687aa7c860b7bc1bd
Travis <travis@travis-ci.org>
2017-10-06 08:32:56 +1100

    changed build.gradle to accomodate for dependencies

--- app/src/test/java/tk/friendar/friendar/clientUnitTest.java
+++ app/src/test/java/tk/friendar/friendar/clientUnitTest.java
@@ -1,10 +1,18 @@
 package tk.friendar.friendar;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by tinba on 6/10/2017.
  */
 
 public class clientUnitTest {
 
+    @Test
+    public void testEmail(){
+
+    }
 
 }
