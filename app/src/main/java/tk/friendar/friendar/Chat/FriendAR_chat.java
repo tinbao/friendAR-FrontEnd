79240f4a1dc0f20b2057ffc9d26a1eb916881c0e
sdi2 <sdi2@student.unimelb.edu.au>
2017-10-13 15:03:01 +1100

    Chat with message bubbles and sender gravity

---
+++ app/src/main/java/tk/friendar/friendar/Chat/FriendAR_chat.java
@@ -0,0 +1,130 @@
+package tk.friendar.friendar.Chat;
+
+import android.content.DialogInterface;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import tk.friendar.friendar.R;
+
+public class FriendAR_chat extends AppCompatActivity implements OnClickListener {
+
+    private EditText msg_edittext;
+    private String user1;
+    private String user2;
+    private Random random;
+    public static ArrayList<ChatMessage> chatlist;
+    public static ChatAdapter chatAdapter;
+    ListView msgListView;
+
+
+    /* set up chat Adapter and chat list*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_layout);
+        Chats chat1 = new Chats();
+        user1 = "Simon";
+        user2 = "Simon1";
+        msg_edittext = (EditText) findViewById(R.id.messageEditText);
+        msgListView = (ListView) findViewById(R.id.msgListView);
+        chatlist = new ArrayList<ChatMessage>();
+        chatAdapter = new ChatAdapter(this, chatlist);
+        ImageButton sendButton = (ImageButton) findViewById(R.id.sendMessageButton);
+        sendButton.setOnClickListener(this);
+
+    }
+
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_friend_ar_chat, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    /*Create current message and add to list and view*/
+    public void sendMessage(View v) {
+        random = new Random();
+
+
+
+        // ----Set autoscroll of listview when a new message arrives----//
+        msgListView.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        msgListView.setStackFromBottom(true);
+
+
+        msgListView.setAdapter(chatAdapter);
+
+        String message = msg_edittext.getEditableText().toString();
+        if (!message.equalsIgnoreCase("")) {
+            final ChatMessage chatMessage = new ChatMessage(user1, user2,
+                    message, "" + random.nextInt(1000), false);
+            chatMessage.setMsgID();
+            chatMessage.body = message;
+            chatMessage.Date = CommonMethods.getCurrentDate();
+            chatMessage.Time = CommonMethods.getCurrentTime();
+            msg_edittext.setText("");
+            chatAdapter.add(chatMessage);
+            chatAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sendMessageButton:
+                sendMessage(v);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
