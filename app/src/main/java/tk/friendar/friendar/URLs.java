da828a8c64b3f3fae1089bb5109c352e0f468cf8
Travis <travis@travis-ci.org>
2017-10-16 16:13:20 +1100

    added chat URL

--- app/src/main/java/tk/friendar/friendar/URLs.java
+++ app/src/main/java/tk/friendar/friendar/URLs.java
@@ -1,14 +1,12 @@
 package tk.friendar.friendar;
 
 /**
- * Created by tinba on 3/10/2017.
+ * Constants of the URLs and endpoints of FriendAR's server
  */
-
 public class URLs {
 
     private static final String ROOT_URL = "http://api.friendar.tk/1/";
 
     public static final String URL_SIGNUP = ROOT_URL + "users";
-    //public static final String URL_SIGNUP = "http://192.168.99.100/1/users";
-    //public static final String URL_LOGIN= ROOT_URL + "login";
+    public static final String URL_CHAT = ROOT_URL + "chat";
 }
