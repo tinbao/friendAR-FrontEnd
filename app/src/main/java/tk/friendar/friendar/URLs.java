2b63c39d7bc146058d46a7bb5508e646280493c0
Travis <travis@travis-ci.org>
2017-10-17 00:56:46 +1100

    added tests for friends

--- app/src/main/java/tk/friendar/friendar/URLs.java
+++ app/src/main/java/tk/friendar/friendar/URLs.java
@@ -11,4 +11,6 @@ public class URLs {
     public static final String URL_SIGNUP = ROOT_URL + "users";
     //public static final String URL_SIGNUP = "http://192.168.99.100/1/users";
     //public static final String URL_LOGIN= ROOT_URL + "login";
+
+    //public static final String URL_MEETING_USERS = ROOT_URL + "meeting"
 }
